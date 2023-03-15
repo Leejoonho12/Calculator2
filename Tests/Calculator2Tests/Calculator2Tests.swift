059da2c76fcf7af1f5ac2ea2d76b5ea68e4fe150
Leejho <h740921@gmail.com>
2023-03-15 14:25:00 +0900

    Initial Commit

---
+++ Tests/Calculator2Tests/Calculator2Tests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import Calculator2
+
+final class Calculator2Tests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(Calculator2().text, "Hello, World!")
+    }
+}
